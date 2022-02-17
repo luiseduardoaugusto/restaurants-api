eebb181c614413d500b2fe2c18151869aa077368
luisaugusto <luis.augusto.u@gmail.com>
2022-02-17 16:14:46 -0500

    mi primera api

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  acts_as_token_authenticatable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :restaurants
+  has_many :comments
+  validates :email, presence: true
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.find_or_initialize_by(email: 'john@rambo.com')
+user.password = 'password12345'
+user.password_confirmation = 'password12345'
+
+restaurant = user.restaurants.find_or_initialize_by(name: 'Bellagamba', address: 'Armenia St')
+restaurant2 = user.restaurants.find_or_initialize_by(name: 'Zitto', address: 'Aristides St')
+
+user.save
